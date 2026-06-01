8c1acded2bfc9552826ca2fc7e871024702f87f3
Rohan Gour <rohanrgour@gmail.com>
2026-06-01 16:47:17 +0530

    Set Filament login route to /login

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('login')
+            ->path('')
             ->login(CustomLogin::class) 
             ->colors([
                 'primary' => Color::Amber,
